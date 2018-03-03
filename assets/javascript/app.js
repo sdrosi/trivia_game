938b37b5ce90949fec46d6b8b2ef0ac7ef4a4da3
Shane Drosi <ShaneDrosi@Joshuas-MacBook-Air.local>
2018-03-02 16:02:40 -0800

    working game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 //variables
 var seconds = 21;
 var countDown;
-var correct;
-var incorrect;
-var missed;
+var correct = 0;
+var incorrect = 0;
+var missed = 0;
 var currentQuestion;
 var answered;
 var userGuess;
@@ -40,22 +40,50 @@ var questionOptions= [
     options: ["Henri Matisse", "Wassily Kandinsky", "Andy Warhol", "Georgia O'Keeffe"],
     answer: 3,
     },
+
+    {
+    question: "Which of the following Abstract Expressionist artists tended to use rectangles of bold, vibrant color?",
+    options: ["Salvador Dali", "Mark Rothko", "Andy Warhol", "Georgia O'Keeffe"],
+    answer: 1,
+    },
+
+    {
+    question: "Which of the following Abstract Expressionist artists tended to use rectangles of bold, vibrant color?",
+    options: ["Salvador Dali", "Mark Rothko", "Andy Warhol", "Georgia O'Keeffe"],
+    answer: 1,
+    },
 ];
 
+//when page loads, hide answer container
+$(document).ready(function() {
+    console.log("READY!");
+    $(".answer-container").hide();
+    $("#results").hide();
 
-//create a button to start game in id instructions
+});
+
+//set ip game with empty arrays, initialize first question
 function setGame() {
     $("questions").empty();
+    $("#results").hide();
+    $("#scoreBoard").hide();
     correct = 0;
-    wrong = 0;
+    incorrect = 0;
     missed = 0;
     currentQuestion = 0;
     answered = 0;
+    seconds = 21;
     addQues();
+    };
 
-};
+
+
+
 //add questions to id questions
 function addQues() {
+    if (currentQuestion < 6) {
+    $(".answer-container").show();
+    $("#scoreBoard").show();
     $("#questions").html("<h3>" + questionOptions[currentQuestion].question + "</h3>");
 //add answer options to id answer-options
     $("#answer-option1").html(questionOptions[currentQuestion].options[0]);
@@ -63,6 +91,12 @@ function addQues() {
     $("#answer-option3").html(questionOptions[currentQuestion].options[2]);
     $("#answer-option4").html(questionOptions[currentQuestion].options[3]);
     choice = questionOptions[currentQuestion].answer;
+    timer();
+    seconds = 21;}
+    //if current question === 7 end game
+    else {
+        endGame();
+    };
     
 };
 
@@ -70,49 +104,67 @@ function addQues() {
 //store user guess as userGuess
 // function addUserGuessClickHandler() {
     $(".guess").on("click", function() {
+        clearInterval(countDown);
+        seconds = 21;
         console.log("You clicked me!");
         console.log(this.value);
         userGuess = this.value;
         console.log("Userguess= " + userGuess);
         console.log(choice);
-        if (choice == userGuess && currentQuestion < 4) {
+        if (choice == userGuess && currentQuestion < 6) {
             console.log("Correct!")
             correct++;
             currentQuestion++;
+            $("#correct").html("<h4>Correct: " + correct + "</h4>");
             addQues();
         }
         else {
-            console.log("nope!");
             incorrect++;
-            addQues();
-            currentQuestion++;
-        }
-
-    });
-
-function timer() {
-    clearInterval(countDown);
-    countDown = setInterval(decrement, 1000);
-};
-
-function decrement() {
-    seconds--;
-    $("#timer").html("Time left: " + seconds)
-    console.log(seconds);
-    if (seconds === 0) {
-        stop(); {
             clearInterval(countDown);
+            seconds = 21;
+            console.log("nope: " + incorrect);
+            currentQuestion++;
+            $("#incorrect").html("<h4>Incorrect: " + incorrect + "</h4>");
+            stop();
+            addQues();
+            }
+        });
+
+        function endGame () {
+            $(".answer-container").hide();
+            $("#results").show();            
         }
-    }
-};
+    
+        //Timing functions
+
+        function timer() {
+            clearInterval(countDown);
+            countDown = setInterval(decrement, 1000);
+        };
+
+        function decrement() {
+            seconds--;
+            $("#timer").html("<h4>Time left: " + seconds + "</h4>")
+            console.log(seconds);
+            if (seconds === 0) {
+                stop(); {
+                    clearInterval(countDown);
+                    currentQuestion = currentQuestion + 1;
+                    console.log(currentQuestion);
+                    addQues();
+                    missed++;
+                    console.log(missed);
+                    $("#missed").html("<h4>Missed: " + missed + "</h4>");
+                }
+            }
+        };
 
        
     
 
-    
+        //
 
-setGame();
-$("#start").on("click", timer);
+$("#start").on("click", setGame);
 
 
 
